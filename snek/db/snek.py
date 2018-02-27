26ce3bbdc67c180485424c41f8b60d4bf77bc878
galperins4 <galperins4@gmail.com>
2018-02-26 19:48:06 -0600

    Update snek.py

--- snek/db/snek.py
+++ snek/db/snek.py
@@ -111,8 +111,8 @@ class SnekDB:
     def unprocessedBlocks(self):
         return self.cursor.execute("SELECT * FROM blocks WHERE processed_at IS NULL ORDER BY height")
     
-    def stagedPayment(self):
-        return self.cursor.execute("SELECT rowid, * FROM staging WHERE processed_at IS NULL LIMIT 50")
+    def stagedPayment(self, b=50):
+        return self.cursor.execute("SELECT rowid, * FROM staging WHERE processed_at IS NULL LIMIT b")
 
     def processStagedPayment(self, rows):
 		
