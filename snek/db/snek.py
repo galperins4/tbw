14f5e97e28561689ab9214e65171861738e87a04
galperins4 <galperins4@gmail.com>
2018-04-06 18:23:03 -0500

    Update snek.py

--- snek/db/snek.py
+++ snek/db/snek.py
@@ -121,8 +121,8 @@ class SnekDB:
 		
         #rows = tuple(rows)
         ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S')		
-        for i in rows:			
-		self.cursor.execute(f"UPDATE staging SET processed_at = '{ts}' WHERE rowid = {i}")	
+        for i in rows:
+	    self.cursor.execute(f"UPDATE staging SET processed_at = '{ts}' WHERE rowid = {i}")	
 
         self.commit()
 	
