b2aa9cc81dd55124418347503ae4df5775733b6e
galperins4 <galperins4@gmail.com>
2018-05-24 19:41:04 -0500

    Update install_beta.sh

--- install_beta.sh
+++ install_beta.sh
@@ -4,7 +4,6 @@ sudo -H pip3 install setuptools
 sudo -H pip3 install -r requirements.txt
 npm install 
 sudo npm install pm2@latest -g
-updatedb
 cd node_modules
 git clone https://github.com/PersonaIam/persona-js
 cd persona-js
