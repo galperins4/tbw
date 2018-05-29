d4336e87c4fff78e8b5ef367c4b0efc53b210ff3
galperins4 <galperins4@gmail.com>
2018-05-28 22:30:26 -0500

    Update ripa_fix.sh

--- ripa_fix.sh
+++ ripa_fix.sh
@@ -1,1 +1,6 @@
 https://github.com/RipaEx/ripa-js
+sudo find / -name "transaction.createTransaction.py" 2>/dev/null | xargs sed -i "s/arkjs/persona-js/g"
+cd node_modules
+git clone https://github.com/PersonaIam/persona-js
+cd persona-js
+npm install
