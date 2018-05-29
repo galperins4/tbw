7294c1fe693e9eb783ccc92e2dd07112acd89b1f
galperins4 <galperins4@gmail.com>
2018-05-28 20:28:39 -0500

    Update persona_fix.sh

--- persona_fix.sh
+++ persona_fix.sh
@@ -1,5 +1,4 @@
-find / -name "transaction.createTransaction.py" 2>/dev/null | xargs sed -i "s/arkjs/persona-js/g"
-
+sudo find / -name "transaction.createTransaction.py" 2>/dev/null | xargs sed -i "s/arkjs/persona-js/g"
 cd node_modules
 git clone https://github.com/PersonaIam/persona-js
 cd persona-js
