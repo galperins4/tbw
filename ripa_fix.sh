028b32baf239ba0cb7cb47a07c263008550bb45f
galperins4 <galperins4@gmail.com>
2018-05-29 22:03:45 -0500

    Delete ripa_fix.sh

--- ripa_fix.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo find / -name "transaction.createTransaction.py" 2>/dev/null | xargs sed -i "s/arkjs/ripa-js/g"
-cd node_modules
-git clone https://github.com/RipaEx/ripa-js
-cd ripa-js
-npm install
